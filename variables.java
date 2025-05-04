c3dda0864b6a35c8f21ce3aff2c5433b88dfe997
247731sathya <mehalaselvi78@gmail.com>
2025-05-04 09:57:38 -0700

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,38 @@
+package variables;
+
+public class variables {
+	
+	public static void main(String[]args)
+	{
+		byte num=100;
+		System.out.println(num);
+		
+		short s = 32000;
+		System.out.println(s);
+		
+		int mynum= 200;
+		System.out.println(mynum);
+		
+		long l=99623822048l;
+		System.out.println(l);
+		
+		float f= 25.12345678f;
+		System.out.println(f);
+		
+		double d  =25.12345678912;
+		System.out.println(d);
+		
+		char c = 'N';
+		System.out.println('N');
+		
+		boolean istrue= true;
+		System.out.println(true);
+		
+		String invitation= "Pravi";
+		System.out.println("Pravi");
+		
+		
+		
+	}
+
+}

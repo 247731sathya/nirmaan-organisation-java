f15c2a0a6faf78b1fde91d685c68fb6567587448
247731sathya <mehalaselvi78@gmail.com>
2025-05-06 11:17:38 +0530

    Add files via upload

---
+++ library.java
@@ -0,0 +1,66 @@
+package day5;
+
+
+public class library {
+
+	int id;
+	String bookname;
+	int price;
+	String authorname;
+	
+	
+    public static void main(String[] args) {
+   //library1
+    library l1 =new library();
+    l1.id=11111;
+    l1.bookname="afrin love story";
+    l1.price=250;
+    l1.authorname="afrin";
+    
+    //library2
+    library l2=new library();
+    l2.id=22222;
+    l2.bookname="afrin breakup story";
+    l2.price=300;
+    l2.authorname="renuka";
+    
+    
+  //library3
+    library l3=new library();
+    l3.id=33333;
+    l3.bookname="afrin crush story";
+    l3.price=350;
+    l3.authorname="megala";
+    
+    
+  //library4
+    library l4=new library();
+    l4.id=44444;
+    l4.bookname="albatross";
+    l4.price=1000;
+    l4.authorname="can";
+    
+  //library5
+    library l5=new library();
+    l5.id=55555;
+    l5.bookname="first time pappu meetup ";
+    l5.price=800;
+    l5.authorname="naveen";
+    
+    
+
+    System.out.println(l1.bookname);  
+    System.out.println(l2.bookname);    
+    System.out.println(l3.bookname);    
+    System.out.println(l4.bookname);    
+    System.out.println(l5.bookname);    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+    }
